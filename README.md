04a012335da50c2a5516f54bd65c1fdd5349a3db
_aubbusta <aubbusta@gmail.com>
2018-11-17 17:52:46 +0100

    BKsmoke Post.php added

---
+++ README.md
@@ -0,0 +1,4 @@
+###A Simple Landing page with a re-Captcha v3 => [see it live](http://bksmoke.com)
+
+
+#### Make sure to change emails and $secret on post.php
--- post.php
+++ post.php
@@ -1,11 +1,70 @@
-<?php
+<?php header("Content-Type: application/json"); ?>
+<?php header('Content-Type: charset=utf-8'); ?><?php
 
 
-$time="";
-$date="";
 
 
-print_r($_POST);
-echo $time."<br>";
-echo $date;
+//Your re-captcha info
+$secret = '********';
+$action = 'action';
+
+$subject = 'Brooklyn Smoke Coming Soon';
+$message = $_POST['message'];
+$from = $_POST['email'];
+$tel = $_POST['tel'];
+$name = $_POST['nom'];
+
+
+
+$err=array();
+
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+
+
+$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+  
+
+
+        if ($responseData->success) {
+
+
+//Send email after Success
+    $to = "testman@gmail.com";
+$txt = " <strong>Nom</strong>: " .$name. "<br> <strong>Email</strong>: ".$from. " <br><strong>Telephone</strong>: ".$tel." <br><strong>Message</strong>: ".$message;
+//$headers = "From: direction@ijmt.tv"."\r\n" ."CC: nouraelallam6@gmail.com";
+$headers =  'From: Brooklyn Smoke <testman@gmail.com>' . "\r\n" ;
+$headers .= 'Cc: metoo@ebsmaroc.com;' . "\r\n";
+// $headers .= 'Bcc: campaign@ebsmaroc.net' . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset: utf8\r\n";
+
+mail($to,$subject,$txt,$headers);
+
+
+            $err['msg']=true;
+
+
+        
+        }else{
+                        
+                        $err['msg']=false;
+                   
+        
+           
+        }
+
+        
+   }else{
+
+      $err['msg']=false;
+
+
+   }
+
+
+   
+
+echo json_encode($err);
+
 ?>
